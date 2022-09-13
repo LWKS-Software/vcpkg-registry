a01f819069205c5a72fe2366fbad8d92366eca8a
TeamLwks <dev@lwks.com>
2022-09-13 12:01:46 +0100

    Fix git-tree hash

--- ports/aja/portfile.cmake
+++ ports/aja/portfile.cmake
@@ -1,15 +1,14 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
-  REPO "aja-video/ntv2"
+  REPO aja-video/ntv2
   REF 93a811d321bc5d2be6e9b8443bfff905cac731cf
-  SHA512 0
+  SHA512 4e2287ddaa345bceb9c9fb4d3e0508de49db2faf67bcbc7c8f0a817d02fb7013cf5572322ad761f230f8ea13c9b79b4744f2b56d1bb72eff045390cfc3d4d165
   HEAD_REF main
 )
 
 vcpkg_configure_cmake(
   SOURCE_PATH "${SOURCE_PATH}"
   PREFER_NINJA
-  OPTIONS -DBUILD_STATIC=OFF -DBUILD_SEPARATED=ON -DBUILD_TOOLS=OFF
 )
 vcpkg_install_cmake()
 vcpkg_fixup_cmake_targets()

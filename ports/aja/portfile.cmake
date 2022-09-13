0e00ff07ce8d518d2c84742d1acbcaf39f9b6492
TeamLwks <dev@lwks.com>
2022-09-13 11:44:58 +0100

    Fix versions

--- ports/aja/portfile.cmake
+++ ports/aja/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO "aja-video/ntv2"
   REF 93a811d321bc5d2be6e9b8443bfff905cac731cf
-  SHA512 4e2287ddaa345bceb9c9fb4d3e0508de49db2faf67bcbc7c8f0a817d02fb7013cf5572322ad761f230f8ea13c9b79b4744f2b56d1bb72eff045390cfc3d4d165
+  SHA512 0
   HEAD_REF main
 )
 

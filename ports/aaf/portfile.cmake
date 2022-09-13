2c622851eda6b9f0f29dd940cfb7f05cd94181e9
TeamLwks <dev@lwks.com>
2022-09-13 11:39:11 +0100

    Adding aaf, aja

--- ports/aaf/portfile.cmake
+++ ports/aaf/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
-  REPO LWKS-Software/cineform-sdk
-  REF bc5118d12921d1c59b95cdcd5d87dc8ac99f1cd0
+  REPO LWKS-Software/AAF
+  REF 73f959bf7783dadbf75db7d8869ebcb387dd262e
   SHA512 4e2287ddaa345bceb9c9fb4d3e0508de49db2faf67bcbc7c8f0a817d02fb7013cf5572322ad761f230f8ea13c9b79b4744f2b56d1bb72eff045390cfc3d4d165
   HEAD_REF main
 )
@@ -9,14 +9,13 @@ vcpkg_from_github(
 vcpkg_configure_cmake(
   SOURCE_PATH "${SOURCE_PATH}"
   PREFER_NINJA
-  OPTIONS -DBUILD_STATIC=OFF -DBUILD_SEPARATED=ON -DBUILD_TOOLS=OFF
-)
+
 vcpkg_install_cmake()
 vcpkg_fixup_cmake_targets()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 file(
-  INSTALL "${SOURCE_PATH}/LICENSE.txt"
+  INSTALL "${SOURCE_PATH}/LEGAL/AAFSDKPSL.TXT"
   DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
   RENAME copyright)
--- ports/aja/portfile.cmake
+++ ports/aja/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
-  REPO LWKS-Software/cineform-sdk
-  REF bc5118d12921d1c59b95cdcd5d87dc8ac99f1cd0
+  REPO "aja-video/ntv2"
+  REF 93a811d321bc5d2be6e9b8443bfff905cac731cf
   SHA512 4e2287ddaa345bceb9c9fb4d3e0508de49db2faf67bcbc7c8f0a817d02fb7013cf5572322ad761f230f8ea13c9b79b4744f2b56d1bb72eff045390cfc3d4d165
   HEAD_REF main
 )
@@ -17,6 +17,6 @@ vcpkg_fixup_cmake_targets()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 file(
-  INSTALL "${SOURCE_PATH}/LICENSE.txt"
+  INSTALL "${SOURCE_PATH}/LICENSE"
   DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
   RENAME copyright)
